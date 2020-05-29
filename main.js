17dc1778fbc87c06e4aa95fbe7109cc98a2df709
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 20:13:16 +0000

    Done.

--- main.js
+++ main.js
@@ -4,13 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 const errorModal = document.querySelector("#modal");
+const errorMessage = errorModal.querySelector("#modal-message");
 errorModal.className = "hidden";
 const likableElements = document.querySelectorAll(".like-glyph");
 likableElements.forEach(likableElement => {
   likableElement.addEventListener("click", () => {
-
+    mimicServerCall()
+    //.then(response => response.json())
+    //.then(json => toggleLiked(json))
+    .then((json) => toggleLiked(json, likableElement))
+    .catch(error => renderError(error));
   })
 });
+
+function toggleLiked(json, likeableElement){
+  //console.log(json);
+  //console.log(likeableElement.className == "activated-heart");
+  if (likeableElement.className == "activated-heart"){
+    likeableElement.innerHTML = EMPTY_HEART;
+    likeableElement.classList.remove("activated-heart");
+  }else{
+    likeableElement.innerHTML = FULL_HEART;
+    likeableElement.className = "activated-heart";
+  }
+}
+
+function renderError(error){
+  errorMessage.innerHTML = error.message;
+  errorModal.classList.remove("hidden");
+  //hide the error modal after 5 seconds:
+  setTimeout(function(){ errorModal.className = "hidden"; }, 5000);
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
