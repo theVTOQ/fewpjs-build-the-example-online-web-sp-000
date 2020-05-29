540aebb64f96ec7e3d85c62a16935dbe2254b5a2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 20:05:22 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ likableElements.forEach(likableElement => {
     //.then(response => response.json())
     //.then(json => toggleLiked(json))
     .then((json) => function(json){
+      console.log(likeableElement.className == "activated-heart");
       if (likeableElement.className == "activated-heart"){
         likeableElement.innerHTML = EMPTY_HEART;
         likeableElement.classList.remove("activated-heart");
